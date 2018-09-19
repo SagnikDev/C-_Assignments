805094cc6b185f22baff24b117db4f3da6bc5147
SagnikDev <sagnikdeveloper@gmail.com>
2018-09-19 19:47:06 +0530

    Inherit updated

--- inheritance.cpp
+++ inheritance.cpp
@@ -11,25 +11,47 @@ public:
 	}
 	
 };
-class Chair :public Table
+class Chair:virtual public Table
 {
-private:
+protected:
 	int height;
 public:
 	void putHeight()
 	{
 		cin>> height;
 	}
+};
+class Door :virtual public Table
+{
+	protected:
+	int width;
+	public:
+	void putWidth()
+	{
+		cin>> width;
+	} 
+};
+class Bed :public Chair,public Door
+{
+protected:
+	int radius;
+	public:
+	void putRadius()
+	{
+		cin>> radius;
+	}
 	void getData()
 	{
-		cout << "The feet is: " << feet << "and the lenth is: " << length << "and the height is: " << height;
-	} 
+		cout << "The feet is: " << feet << "and the lenth is: " << length << "and the height is: " << height << "and the width is: " << width<< "and the radius is: " << radius<< "\n";
+	}  
 };
 int main()
 {
-	Chair c1;
+	Bed c1;
 	c1.putData();
 	c1.putHeight();
+	c1.putWidth();
+	c1.putRadius();
 	c1.getData();
 	return 0;
 }
