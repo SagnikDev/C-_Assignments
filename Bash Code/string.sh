16c5de5bfc7458a7f2d5abad15e87b9cec341452
SagnikDev <sagnikroy009@gmail.com>
2018-10-03 05:28:26 +0530

    Algorithm Program Added

--- Bash Code/string.sh
+++ Bash Code/string.sh
@@ -6,4 +6,5 @@ echo "The concatinate string is: $str3"
 echo "The number of characters in $str3 is ${#str3}"
 read -p "Enter the starting position to cut the string" start
 read -p "Enter the length of the string" length
-echo "The cutted string is ${str3:$start:$length}"
+echo "The cutted string is ${str3:$start:$length
+}"
